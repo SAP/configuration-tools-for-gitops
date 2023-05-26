598335db7f25b817a41ea5d2c546af46ed6a89aa
blackhatcrazy <44291985+blackhatcrazy@users.noreply.github.com>
2023-05-26 18:54:19 +0200

    chore(cmd): refactor commands (#24)
    * chore(cmd): restructure cmd (remove init functions)
    
    * fix(semantic-release): remove flag to make it run?
    
    ---------
    
    Co-authored-by: I503834 <jan.fokken@sap.com>

--- cmd/coco/commands/graph.go
+++ cmd/coco/commands/graph.go
@@ -9,11 +9,12 @@ import (
 	"github.com/spf13/viper"
 )
 
-var graphCmd = &cobra.Command{
-	Use:     "graph",
-	Aliases: []string{"g"},
-	Short:   "Returns the downstream dependencies for all components in the repository",
-	Long: `This command constructs all components that depend on any given component.
+func newGraph() *cobra.Command {
+	return &cobra.Command{
+		Use:     "graph",
+		Aliases: []string{"g"},
+		Short:   "Returns the downstream dependencies for all components in the repository",
+		Long: `This command constructs all components that depend on any given component.
 	This is done by constructing the full dependency graph of upstream components
 	(defined implicitly via the direct upstream dependencies given in the 
 	dependencies.yaml file of each component) and then inverting this graph.
@@ -21,21 +22,16 @@ var graphCmd = &cobra.Command{
 	where the weight corresponds to the number of connections to reach the downstream
 	from the upstream component.
 	`,
-	Run: func(cmd *cobra.Command, args []string) {
-		deps, _, err := dependencies.Graph(
-			viper.GetString(gitPath),
-			dependencyFile,
-		)
-		if err != nil {
-			log.Sugar.Errorf("graph failed with: %s", err)
-			os.Exit(1)
-		}
-		deps.Print(os.Stdout, format)
-
-	},
-}
-
-//nolint:gochecknoinits // required by the cobra framework
-func init() {
-	dependenciesCmd.AddCommand(graphCmd)
+		Run: func(cmd *cobra.Command, args []string) {
+			deps, _, err := dependencies.Graph(
+				viper.GetString(gitPath),
+				viper.GetString(componentCfg),
+			)
+			if err != nil {
+				log.Sugar.Errorf("graph failed with: %s", err)
+				os.Exit(1)
+			}
+			deps.Print(os.Stdout, format)
+		},
+	}
 }
