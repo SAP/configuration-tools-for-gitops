8ca4513d5c57eaa23b3a7239d22f8ba8e540eaed
blackhatcrazy <44291985+blackhatcrazy@users.noreply.github.com>
2023-06-01 11:36:17 +0200

    feat(generate): use sprig functions in templates (#28)
    * switch to sprig for templating
    
    * update dependencies
    
    ---------
    
    Co-authored-by: I503834 <jan.fokken@sap.com>

--- cmd/coco/generate/templatefunctions.go
+++ cmd/coco/generate/templatefunctions.go
@@ -1,38 +1,30 @@
 package generate
 
 import (
-	"fmt"
 	"strings"
 	gotemplate "text/template"
 
-	"github.com/google/uuid"
+	"github.com/Masterminds/sprig"
 )
 
 func tmplFuncs() gotemplate.FuncMap {
 	// https://www.calhoun.io/intro-to-templates-p3-functions/
 	// https://golang.org/pkg/text/template/#FuncMap
-	return gotemplate.FuncMap{
-		"trimSuffix": strings.TrimSuffix,
-		"trimPrefix": strings.TrimPrefix,
-		"join": func(sep string, elems ...string) string {
-			res := make([]string, 0, len(elems))
-			for _, e := range elems {
-				if e != "" {
-					res = append(res, e)
-				}
-			}
-			return strings.Join(res, sep)
-		},
-		"split": strings.Split,
-		"quote": func(s string) string {
-			return fmt.Sprintf("%q", s)
-		},
-		"select": func(el int, sl []string) string {
-			if el >= len(sl) {
-				return ""
-			}
-			return sl[el]
-		},
-		"uuid4": uuid.NewString,
+	funcMaps := sprig.FuncMap()
+	funcMaps["select"] = func(el int, sl []string) string {
+		if el >= len(sl) {
+			return ""
+		}
+		return sl[el]
 	}
+	funcMaps["joinElems"] = func(sep string, elems ...string) string {
+		res := make([]string, 0, len(elems))
+		for _, e := range elems {
+			if e != "" {
+				res = append(res, e)
+			}
+		}
+		return strings.Join(res, sep)
+	}
+	return funcMaps
 }
